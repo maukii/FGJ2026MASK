1a26f26a97132d2ed25d6e74ebca7724a9f29ce8
Mauno Kiiskinen <31925951+maukii@users.noreply.github.com>
2026-02-01 02:16:16 +0200

    tween health UI

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public event Action OnGameOver;
+
     [SerializeField] private GameTimer gameTimer;
     [SerializeField] private RoundTimer roundTimer;
     [SerializeField] private Light2D globalLight;
@@ -11,11 +16,34 @@ public class GameController : MonoBehaviour
     [SerializeField] private float lightFadeDuration = 1f;
     [SerializeField] private SpriteRenderer lightBeam;
     [SerializeField] private Light2D pointLight;
+    [SerializeField] private List<Image> healthParentGraphics = new List<Image>();
+    [SerializeField] private List<Image> healthGraphics = new List<Image>();
+
+    private int currentHealth;
 
 
     void Awake()
     {
+        currentHealth = 3;
+
         DollController.OnTutorialCompleted += TutorialCompleted;
+        DollController.OnWrongDollKicked += LoseLife;
+    }
+
+    private void LoseLife()
+    {
+        currentHealth--;
+
+        healthParentGraphics[currentHealth].transform.DOPunchScale(Vector3.one * 0.25f, 0.35f);
+        healthParentGraphics[currentHealth].transform.DOPunchRotation(Vector3.forward, 0.25f);
+
+        healthGraphics[currentHealth].transform.DOScale(0f, 0.25f).SetEase(Ease.OutExpo);
+        healthGraphics[currentHealth].transform.DOLocalRotate(new Vector3(0f, 0f, -360f), 0.25f, RotateMode.FastBeyond360).SetEase(Ease.OutExpo);
+
+        if (currentHealth <= 0)
+        {
+            OnGameOver?.Invoke();
+        }
     }
 
     private void TutorialCompleted()
